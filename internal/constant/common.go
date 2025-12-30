9bc4d169556ffd23a6c44fcca2d6606b3a5949e9
cern <amadashunli@hotmail.com>
2025-12-30 11:00:57 +0800

    [enhance] update const value

--- internal/constant/common.go
+++ internal/constant/common.go
@@ -1,7 +1,9 @@
 package constant
 
 const (
+	DefaultArch                = "amd64"
 	DefaultNerdctlVersion      = "2.1.0"
 	DefaultContainerdVersion   = "2.1.0"
+	DefaultContainerdArch      = "amd64"
 	DefaultDockerEngineVersion = "27.5.1"
 )
